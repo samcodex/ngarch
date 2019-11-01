2b346c3ed09ba5506c65026f553325e36fc869ca
samcodex <xie.samuel@gmail.com>
2019-11-01 07:44:06 -0400

    Add getBootstrappedComponent

--- src/app/core/arch-ngponent/arch-ngponent-helper.ts
+++ src/app/core/arch-ngponent/arch-ngponent-helper.ts
@@ -3,6 +3,8 @@ import { ClassProvider, FactoryProvider } from '@angular/core';
 import { TsPonent } from '../ngponent-tsponent/tsponent';
 import { TsPonentType } from '../ngponent-tsponent/tsponent-definition';
 import { ArchNgPonent } from '@core/arch-ngponent/arch-ngponent';
+import { ArchNgPonentModule } from '@core/arch-ngponent/arch-ngponent-module';
+import { ArchNgPonentComponent } from '@core/arch-ngponent/arch-ngponent-component';
 
 export namespace archNgPonentHelper {
   /**
@@ -21,6 +23,7 @@ export namespace archNgPonentHelper {
 
   export const findCallExpressionPonent = _findCallExpressionPonent;
   export const findIdentifierExpressionPonent = _findIdentifierExpressionPonent;
+  export const getBootstrappedComponent = _getBootstrappedComponent;
 }
 
 // TsPonentType.CallExpressionPonent
@@ -36,4 +39,10 @@ function _findSpecificPonent(ponent: TsPonent | ArchNgPonent,
     ponentType: TsPonentType, callerName?: string): TsPonent {
   const tsPonent: TsPonent = ponent instanceof ArchNgPonent ? ponent.ngPonent.getTsPonent() : ponent;
   return tsPonent.findMemberWithTypeAndName(ponentType, callerName);
+}
+
+function _getBootstrappedComponent(ngModule: ArchNgPonentModule): ArchNgPonentComponent[] {
+  const bootstraps: string[] = ngModule.getMetadataOf('bootstrap');
+  const bootstrappedComponents: ArchNgPonentComponent[] = bootstraps.map(comName => ngModule.allDependencies.find(dep => dep.name === comName));
+  return bootstrappedComponents;
 }
